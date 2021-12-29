a1a2e0d992f0513a55102dea82c2d6ee02276150
Jpford93 <jpettiford35@gmail.com>
2021-12-29 15:44:42 -0500

    New JS and more

---
+++ test.js
@@ -0,0 +1,20 @@
+function randomParty() {
+    let facts = Math.floor(Math.random() * (sentences.length));
+    return sentences[facts];
+}
+
+let sentences = [
+    'I started coding in 2008',
+    'I have been graphic designing for 11 years.',
+    'My mother inspired to get into fashion.',
+]
+
+function getKnow() {
+    let canvas = document.getElementById("funFact");
+    let context = canvas.getContext("2d");
+    context.clearRect(0, 0, canvas.width, canvas.height);
+    context.font = "20px Arial";
+    context.fillStyle = "white";
+    context.textAlign = "center";
+    context.fillText(randomParty(), 250, canvas.height / 2 + 10);
+}
